7ae32a1bbc7d6c2bf9d8ecd058ce78ebdabf2270
Prithvish Raj Shukla <157020594+prithvishshukla@users.noreply.github.com>
2025-01-14 02:25:18 +0530

    Create leap year finder code.c

---
+++ leap year finder code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int year;
+    printf("enter year: \n");
+    scanf("%d", &year);
+    if ((year %4==0 && year%100!=0) || year%400==0){
+        printf("this is leap year\n");
+    }
+    else
+           printf("this is not leap year\n");
+    return 0;
+}
