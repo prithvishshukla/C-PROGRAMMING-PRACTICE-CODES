cd27a2517694a065eec17333658a5f332c6c2b5e
Prithvish Raj Shukla <157020594+prithvishshukla@users.noreply.github.com>
2025-01-14 01:57:24 +0530

    Create sub and sum in c.c

---
+++ sub and sum in c.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+int a, b;
+    printf("value of a: \n " );
+    scanf("%d",&a);
+        printf("value of b: \n " );
+            scanf("%d",&b);
+int sub = a-b;
+printf("sub = %d \n", sub);
+int sum = a+b;
+printf("sum = %d \n", sum);
+return 0;
+}
