0e6d16c4268097530c56ca715ed9bcfd5ba762ee
Prithvish Raj Shukla <157020594+prithvishshukla@users.noreply.github.com>
2025-01-14 00:37:59 +0530

    .

---
+++ if else practice.c
@@ -0,0 +1,31 @@
+// if waala
+#include <stdio.h>
+int main() {
+   int l = 5;
+   if (l > 2){
+    printf("hogya solve");
+   }
+   else {
+    printf("nahi hua");
+    }
+   return 0;
+}
+
+
+
+
+
+
+// else waala
+
+// #include <stdio.h>
+// int main() {
+//    int l = 5;
+//    if (l > 9){
+//     printf("hogya solve");
+//    }
+//    else {
+//     printf("nahi hua");
+//     }
+//    return 0;
+// }
