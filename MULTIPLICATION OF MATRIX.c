1845553c583de053a96e3f35c626bd4b70a8f2b5
Prithvish Raj Shukla <157020594+prithvishshukla@users.noreply.github.com>
2025-01-13 15:10:38 +0530

    Update MULTIPLICATION OF MATRIX.c

--- MULTIPLICATION OF MATRIX.c
+++ MULTIPLICATION OF MATRIX.c
@@ -1,71 +1,17 @@
 #include <stdio.h>
-
-#define MAX 10
-
-void multiplyMatrices(int firstMatrix[MAX][MAX], int secondMatrix[MAX][MAX], int resultMatrix[MAX][MAX], int rowFirst, int columnFirst, int columnSecond) {
-    int i, j, k;
-
-    // Initialize the result matrix with 0
-    for (i = 0; i < rowFirst; ++i) {
-        for (j = 0; j < columnSecond; ++j) {
-            resultMatrix[i][j] = 0;
-        }
-    }
-
-    // Multiply matrices
-    for (i = 0; i < rowFirst; ++i) {
-        for (j = 0; j < columnSecond; ++j) {
-            for (k = 0; k < columnFirst; ++k) {
-                resultMatrix[i][j] += firstMatrix[i][k] * secondMatrix[k][j];
-            }
-        }
-    }
-}
-
 int main() {
-    int firstMatrix[MAX][MAX], secondMatrix[MAX][MAX], resultMatrix[MAX][MAX];
-    int rowFirst, columnFirst, rowSecond, columnSecond;
-    int i, j;
-
-    printf("Enter rows and columns for the first matrix: ");
-    scanf("%d %d", &rowFirst, &columnFirst);
-
-    printf("Enter rows and columns for the second matrix: ");
-    scanf("%d %d", &rowSecond, &columnSecond);
-
-    // Check if matrix multiplication is possible
-    if (columnFirst != rowSecond) {
-        printf("Matrix multiplication is not possible.\n");
-        return -1;
-    }
-
-    // Input elements of the first matrix
-    printf("Enter elements of the first matrix:\n");
-    for (i = 0; i < rowFirst; ++i) {
-        for (j = 0; j < columnFirst; ++j) {
-            scanf("%d", &firstMatrix[i][j]);
-        }
-    }
-
-    // Input elements of the second matrix
-    printf("Enter elements of the second matrix:\n");
-    for (i = 0; i < rowSecond; ++i) {
-        for (j = 0; j < columnSecond; ++j) {
-            scanf("%d", &secondMatrix[i][j]);
-        }
-    }
-
-    // Call the function to multiply matrices
-    multiplyMatrices(firstMatrix, secondMatrix, resultMatrix, rowFirst, columnFirst, columnSecond);
-
-    // Output the result matrix
-    printf("Resultant Matrix:\n");
-    for (i = 0; i < rowFirst; ++i) {
-        for (j = 0; j < columnSecond; ++j) {
-            printf("%d ", resultMatrix[i][j]);
-        }
-        printf("\n");
-    }
-
-    return 0;
+int A[2][2] = {{1, 2}, {3, 4}};
+int B[2][2] = {{5, 6}, {7, 8}};
+int C[2][2] = {0};
+printf("Matrix Multiplication:\n");
+for (int i = 0; i < 2; i++) {
+for (int j = 0; j < 2; j++) {
+for (int k = 0; k < 2; k++) {
+C[i][j] += A[i][k] * B[k][j];
+}
+printf("%d ", C[i][j]);
+}
+printf("\n");
+}
+return 0;
 }
