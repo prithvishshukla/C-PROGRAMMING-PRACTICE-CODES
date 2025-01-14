b013040b16675b213b7d8a7d4c93ff01ccf0ba3e
Prithvish Raj Shukla <157020594+prithvishshukla@users.noreply.github.com>
2025-01-14 23:52:40 +0530

    .

--- I love you n times (just for fun).c
+++ I love you n times (just for fun).c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 // I love you n times (just for fun)
-// application of while loop
 int main(){
     int n,i=0;
-    printf("No of time:\n");
+    printf("No of times:\n");
      scanf("%d", &n);
      while (i < n){
-    printf("I love you:%d\n", i+1);
+    printf("I love you %d times\n", i+1);
      i = i + 1;
      }
     return 0;
