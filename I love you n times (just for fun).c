4557e30a84c91017427297d7337e32d93de0d210
Prithvish Raj Shukla <157020594+prithvishshukla@users.noreply.github.com>
2025-01-14 23:29:25 +0530

    .

---
+++ I love you n times (just for fun).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// I love you n times (just for fun)
+int main(){
+    int n,i=0;
+    printf("No of time:\n");
+     scanf("%d", &n);
+     while (i < n){
+    printf("I love you:%d\n", i+1);
+     i = i + 1;
+     }
+    return 0;
+}
