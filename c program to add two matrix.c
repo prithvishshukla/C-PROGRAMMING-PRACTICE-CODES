8e9d9155085560f78cae0716363906a76bddb39a
Prithvish Raj Shukla <157020594+prithvishshukla@users.noreply.github.com>
2025-01-19 21:51:47 +0530

    .

--- c program to add two matrix.c
+++ c program to add two matrix.c
@@ -1,15 +1,49 @@
 #include <stdio.h>
+
 int main() {
-int A[2][2] = {{1, 2}, {3, 4}};
-int B[2][2] = {{5, 6}, {7, 8}};
-int C[2][2];
-printf("Matrix Addition:\n");
-for (int i = 0; i < 2; i++) {
-for (int j = 0; j < 2; j++) {
-C[i][j] = A[i][j] + B[i][j];
-printf("%d ", C[i][j]);
-}
-printf("\n");
-}
-return 0;
+    int N;
+    printf("Enter the order of the matrices: ");
+    scanf("%d", &N);
+
+    int A[N][N], B[N][N], C[N][N];
+
+    printf("Enter elements of first matrix:\n");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    printf("Enter elements of second matrix:\n");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    // Initialize the result matrix to 0
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            C[i][j] = 0;
+        }
+    }
+
+    // Matrix multiplication
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k < N; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    printf("Matrix Multiplication:\n");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
